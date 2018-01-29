c80b4cf8b0a4c8915a674740a83b1b4a927753ff
Kiril <kpanar82@gmail.com>
2018-01-29 13:18:48 +0300

    first commit

---
+++ Имя/script.js
@@ -0,0 +1,4 @@
+var a = prompt("Введите своё имя: ");
+//document.write("Hello "+ a);
+//console.log("Hello "+ a);
+alert("Hello "+ a);
---
+++ Калькулятор/index.js
@@ -0,0 +1,17 @@
+var firstNumber;
+var secondNumber;
+
+firstNumber = Number(prompt("input first number"));
+secondNumber = Number(prompt("input first number"));
+
+var sum = firstNumber + secondNumber;
+var number = firstNumber / secondNumber;
+var a = firstNumber * secondNumber;
+var b = firstNumber - secondNumber;
+
+console.log(sum);
+
+document.write("sum= " + sum + "</br>");
+document.write("sum= " + number + "</br>");
+document.write("sum= " + a + "</br>");
+document.write("sum= " + b);
